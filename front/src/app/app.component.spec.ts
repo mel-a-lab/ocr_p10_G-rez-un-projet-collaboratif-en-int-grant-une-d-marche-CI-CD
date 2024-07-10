c4ae9bf638d31ec01651ed7443a736a5e742efcd
Mélanie <melanie@estorik.com>
2024-07-10 12:27:25 +0200

    Ajout imports dans app.component.spec.ts

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,13 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {JokesService} from "./services/jokes.service";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { JokesService } from './services/jokes.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDividerModule
+      ],
       providers: [JokesService]
     }).compileComponents();
   });
